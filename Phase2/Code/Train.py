21e04a65cccafae9b0cc1d2c08c31586fb454fd5
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-02-19 09:27:18 -0500

    Added logging for first sample predictions every epoch

--- Phase2/Code/Train.py
+++ Phase2/Code/Train.py
@@ -55,6 +55,8 @@ class TrainSupervised():
         self.PatchB_validation_data = None
         self.H4_validation_data = None
         self.H_AB_validation_data = None
+        self.train_H4_data = np.empty([0, 8])
+        self.val_H4_data = np.empty([0, 8])
 
     def fit(self):
 
@@ -74,6 +76,9 @@ class TrainSupervised():
                 grads = tape.gradient(loss, self.Model.trainable_variables)
                 self.Optimizer.apply_gradients(zip(grads, self.Model.trainable_variables))
 
+            if(running_loss == 0.0):
+                self.train_H4_data = np.insert(self.train_H4_data, len(self.train_H4_data), y_[0].numpy(), axis=0)
+
             # print("y_: ", y_)
             # print("y: ", y)
             # print("Loss: ", loss)
@@ -82,7 +87,7 @@ class TrainSupervised():
             running_loss += loss
             # self.L2_loss = running_loss
 
-        result = generateResult(self.TrainingData[0], deprocess_H4_data(y_[0].numpy()), self.H_AB_training_data[0], [240, 320], [128,128], 32, False)
+        result = generateResult(self.TrainingData[0], deprocess_H4_data(self.train_H4_data[-1]), self.H_AB_training_data[0], [240, 320], [128,128], 32, False)
         cv2.imwrite(os.path.join(self.ResultsPath, 'Train', '%d.png'%self.Epoch), result)
         running_loss = running_loss/(len(self.PatchA_training_data)//self.BatchSize) # Mean loss of the epoch
         print("Training Loss: %f"%(running_loss))
@@ -104,10 +109,12 @@ class TrainSupervised():
             y_ = self.Model(X, training=False) # Prediction of the network
             loss = L2_loss(y, y_) # L2 loss between prediction (y_) and ground truth (y)
 
+            if(running_loss == 0.0):
+                self.val_H4_data = np.insert(self.val_H4_data, len(self.val_H4_data), y_[0].numpy(), axis=0)
+
             running_loss += loss
 
-        result = generateResult(self.ValidationData[0], deprocess_H4_data(y_[0].numpy()), self.H_AB_validation_data[0], [240, 320], [128,128], 32, False)
-
+        result = generateResult(self.ValidationData[0], deprocess_H4_data(self.val_H4_data[-1]), self.H_AB_validation_data[0], [240, 320], [128,128], 32, False)
         cv2.imwrite(os.path.join(self.ResultsPath, 'Val', '%d.png'%self.Epoch), result)
         running_loss = running_loss/(len(self.PatchA_validation_data)/self.BatchSize) # Mean loss of the epoch
         print("Validation Loss: %f"%(running_loss))
@@ -149,8 +156,8 @@ class TrainSupervised():
 
         learning_rate = self.LearningRate # Learning rate of 0.005 from the paper
         momentum = 0.9 # Momentum of SGD from the paper
-        self.Optimizer = SGD(momentum=momentum, learning_rate=learning_rate) # Using Stochastic Gradient Descent as optimizer for training
-        # self.Optimizer = Adam(learning_rate=learning_rate)
+        # self.Optimizer = SGD(momentum=momentum, learning_rate=learning_rate) # Using Stochastic Gradient Descent as optimizer for training
+        self.Optimizer = Adam(learning_rate=learning_rate)
 
         # Creating a Checkpoint Manager
         ckpt = Checkpoint(step=tf.Variable(0), model=self.Model, optimizer=self.Optimizer)
@@ -217,10 +224,16 @@ class TrainSupervised():
                 # data = [epoch, np.mean(self.Optimizer.lr.numpy()), np.mean(training_epoch_loss.numpy()), 0.0]
                 if(epoch == 1):
                     df = pd.DataFrame([data], columns = ['Epochs', 'Learning Rate','Training Loss', 'Validation Loss'])
-                    df.to_csv(os.path.join(self.TrainingDir, 'model_state.csv'), mode='a')
+                    df.to_csv(os.path.join(self.TrainingDir, 'Training.csv'), mode='a')
                 else:
                     df = pd.DataFrame([data])
-                    df.to_csv(os.path.join(self.TrainingDir, 'model_state.csv'), header=False, mode='a')
+                    df.to_csv(os.path.join(self.TrainingDir, 'Training.csv'), header=False, mode='a')
+
+                with open (os.path.join(self.TrainingDir, 'Train_H4.npy'), 'wb') as f:
+                    np.save(f, self.train_H4_data)
+
+                with open (os.path.join(self.TrainingDir, 'Val_H4.npy'), 'wb') as f:
+                    np.save(f, self.val_H4_data)
 
                 x.append(epoch)
                 y1.append(training_epoch_loss)
@@ -235,6 +248,13 @@ class TrainSupervised():
             print("Took %.03f minutes to train"%((end-start)/60))
         except KeyboardInterrupt:
             print(traceback.format_exc())
+
+            with open (os.path.join(self.TrainingDir, 'Train_H4.npy'), 'wb') as f:
+                np.save(f, self.train_H4_data)
+
+            with open (os.path.join(self.TrainingDir, 'Val_H4.npy'), 'wb') as f:
+                np.save(f, self.val_H4_data)
+
             save_path = ckpt_manager.save()
             print("Saved checkpoint for step {}: {}".format(int(ckpt.step), save_path))
 
@@ -248,7 +268,7 @@ def main():
     Parser.add_argument('--DatasetPath', type=str, default="../Data/Patches_5000/", help='Path to the training and validation dataset')
     Parser.add_argument('--Epochs', type=int, default=200, help='Epochs to train the model for')
     Parser.add_argument('--BatchSize', type=int, default=64, help='batch_size')
-    Parser.add_argument('--LearningRate', type=float, default=0.005, help='Learning rate for the optimizer')
+    Parser.add_argument('--LearningRate', type=float, default=0.001, help='Learning rate for the optimizer')
     Parser.add_argument('--TrainingDir', type=str, default="../Training/", help='Path to save the training and validation results')
     Parser.add_argument('--CheckpointPath', type=str, default=None, help='Checkpoint for inference/resuming training')
     Parser.add_argument('--CheckpointEpoch', type=int, default=0, help='Checkpoint epoch to resume training from')
