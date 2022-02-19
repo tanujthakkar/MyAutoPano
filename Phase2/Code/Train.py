Switched to SGD, corrected running_loss calculation

Phase2/Code/Train.py
@@ -31,13 +31,15 @@ np_config.enable_numpy_behavior()
 
 class TrainSupervised():
 
-    def __init__(self, DatasetPath, Epochs, BatchSize, TrainingDir, CheckpointPath=None, CheckpointEpoch=None):
+    def __init__(self, DatasetPath, Epochs, BatchSize, LearningRate, TrainingDir, CheckpointPath=None, CheckpointEpoch=None, ExperimentName=None):
         self.DatasetPath = DatasetPath
         self.Epochs = Epochs + 1
         self.BatchSize = BatchSize
+        self.LearningRate = LearningRate
         self.TrainingDir = TrainingDir
         self.CheckpointPath = CheckpointPath
         self.CheckpointEpoch = CheckpointEpoch
+        self.ExperimentName = ExperimentName
 
         self.Model = None
         self.Optimizer = None
@@ -72,35 +74,18 @@ class TrainSupervised():
                 grads = tape.gradient(loss, self.Model.trainable_variables)
                 self.Optimizer.apply_gradients(zip(grads, self.Model.trainable_variables))
 
+            # print("y_: ", y_)
+            # print("y: ", y)
+            # print("Loss: ", loss)
+            # input('q')
+
             running_loss += loss
             # self.L2_loss = running_loss
 
-            # if(batch%1000 == 0):
-            # # if(True):
-            #     print("L2 Loss: ", loss)
-            #     # print("Model Trainable Vars: ", self.Model.trainable_variables)
-            #     print((batch*self.BatchSize)+index)
-            #     cv2.imshow("A", np.float32(cv2.normalize(cv2.imread(self.PatchA_training_data[(batch*self.BatchSize)+index]), None, 0., 1.0, cv2.NORM_MINMAX)))
-            #     cv2.imshow("B", preprocess_image(load_image(self.PatchB_training_data[(batch*self.BatchSize)+index])))
-            #     print(self.H4_training_data[(batch*self.BatchSize)+index])
-            #     print(y)
-            #     print(y_)
-            #     print(deprocess_H4_data(y_.numpy()))
-            #     diff = deprocess_H4_data(y_.numpy()) - self.H4_training_data[(batch*self.BatchSize)+index]
-            #     print(diff)
-            #     print(diff**2)
-            #     print(np.sum(diff**2)/len(diff**2))
-            #     diff_ = y_- y
-            #     print(diff_)
-            #     print(diff_**2)
-            #     print(len(diff_[0]))
-            #     print(np.sum(diff_**2)/len(diff_[0]))
-            #     print(tf.reduce_sum((y_ - y)**2)/8)
-            
-        # result = generateResult(self.TrainingData[(batch*self.BatchSize)+index], deprocess_H4_data(y_.numpy()), self.H_AB_training_data[batch], [240, 320], [128,128], 32, False)
-        # cv2.imwrite(os.path.join(self.ResultsPath, 'Train', '%d.png'%self.Epoch), result)
-        running_loss = running_loss/len(self.PatchA_training_data) # Mean loss of the epoch
-        print("Training Loss: %f %f"%(running_loss, (32*np.sqrt(running_loss))))
+        result = generateResult(self.TrainingData[0], deprocess_H4_data(y_[0].numpy()), self.H_AB_training_data[0], [240, 320], [128,128], 32, False)
+        cv2.imwrite(os.path.join(self.ResultsPath, 'Train', '%d.png'%self.Epoch), result)
+        running_loss = running_loss/(len(self.PatchA_training_data)//self.BatchSize) # Mean loss of the epoch
+        print("Training Loss: %f"%(running_loss))
 
         return running_loss
 
@@ -121,10 +106,11 @@ class TrainSupervised():
 
             running_loss += loss
 
-            # result = generateResult(self.ValidationData[int(len(self.PatchA_validation_data)/4)-1], deprocess_H4_data(y_.numpy()), self.H_AB_validation_data[batch], [240, 320], [128,128], 32, True)
-        # cv2.imwrite(os.path.join(self.ResultsPath, 'Val', '%d.png'%self.Epoch), result)
-        running_loss = running_loss/len(self.PatchA_validation_data) # Mean loss of the epoch
-        print("Validation Loss: %f %f"%(running_loss, (32*np.sqrt(running_loss))))
+        result = generateResult(self.ValidationData[0], deprocess_H4_data(y_[0].numpy()), self.H_AB_validation_data[0], [240, 320], [128,128], 32, False)
+
+        cv2.imwrite(os.path.join(self.ResultsPath, 'Val', '%d.png'%self.Epoch), result)
+        running_loss = running_loss/(len(self.PatchA_validation_data)/self.BatchSize) # Mean loss of the epoch
+        print("Validation Loss: %f"%(running_loss))
 
         return running_loss
 
@@ -145,7 +131,9 @@ class TrainSupervised():
 
             StartEpoch = self.CheckpointEpoch + 1 # Initializing starting epoch of the training operation
         else:
-            self.TrainingDir = os.path.join(self.TrainingDir, datetime.now().strftime("%Y%m%d-%H%M%S")) # Initializing the training directory for current training instance
+            if(not self.ExperimentName):
+                self.ExperimentName = datetime.now().strftime("%Y%m%d-%H%M%S")
+            self.TrainingDir = os.path.join(self.TrainingDir, self.ExperimentName) # Initializing the training directory for current training instance
             os.makedirs(self.TrainingDir)
             self.CheckpointPath = os.path.join(self.TrainingDir, 'Checkpoints') # Initializing checkpoint directory of current training instance
             os.makedirs(self.CheckpointPath)
@@ -159,10 +147,10 @@ class TrainSupervised():
 
             StartEpoch = 1 # Initializing starting epoch of the training operation
 
-        learning_rate = 5E-5 # Learning rate of 0.005 from the paper
+        learning_rate = self.LearningRate # Learning rate of 0.005 from the paper
         momentum = 0.9 # Momentum of SGD from the paper
-        # opt = SGD(momentum=momentum, learning_rate=learning_rate) # Using Stochastic Gradient Descent as optimizer for training
-        self.Optimizer = Adam(learning_rate=learning_rate)
+        self.Optimizer = SGD(momentum=momentum, learning_rate=learning_rate) # Using Stochastic Gradient Descent as optimizer for training
+        # self.Optimizer = Adam(learning_rate=learning_rate)
 
         # Creating a Checkpoint Manager
         ckpt = Checkpoint(step=tf.Variable(0), model=self.Model, optimizer=self.Optimizer)
@@ -196,6 +184,7 @@ class TrainSupervised():
             print("Restored Learnign Rate: {}".format(self.Optimizer.lr))
         else:
             print("Initializing from scratch.")
+            print("Learnign Rate: {}".format(self.Optimizer.lr))
 
 
         start = time.time() # Training instance start time
@@ -209,13 +198,13 @@ class TrainSupervised():
                 print("Epoch %d of %d"%((epoch), self.Epochs-1))
                 self.Epoch = epoch # Updating current epoch
 
-                if((epoch*len(self.PatchA_training_data))%2000000 == 0):
+                if((epoch*(len(self.PatchA_training_data)//self.BatchSize))%1000 == 0):
                     learning_rate = learning_rate / 2
                     print("Updated Learning Rate: %f"%learning_rate)
                     self.Optimizer.lr.assign(learning_rate)
 
-                # training_epoch_loss = self.fit()
-                training_epoch_loss = np.zeros(1)
+                training_epoch_loss = self.fit()
+                # training_epoch_loss = np.zeros(1)
                 validation_epoch_loss = self.validate()
                 # validation_epoch_loss = 0.0
 
@@ -256,24 +245,28 @@ class TrainSupervised():
 def main():
     Parser = argparse.ArgumentParser()
     Parser.add_argument('--ModelType', type=str, default="Supervised", help='Select between Supervised and Unsupervised model for training (Supervised, Unsupervised)')
-    Parser.add_argument('--DatasetPath', type=str, default="../Data/Patches/", help='Path to the training and validation dataset')
-    Parser.add_argument('--Epochs', type=int, default=90000, help='Epochs to train the model for')
-    Parser.add_argument('--BatchSize', type=int, default=1, help='batch_size')
+    Parser.add_argument('--DatasetPath', type=str, default="../Data/Patches_5000/", help='Path to the training and validation dataset')
+    Parser.add_argument('--Epochs', type=int, default=200, help='Epochs to train the model for')
+    Parser.add_argument('--BatchSize', type=int, default=64, help='batch_size')
+    Parser.add_argument('--LearningRate', type=float, default=0.005, help='Learning rate for the optimizer')
     Parser.add_argument('--TrainingDir', type=str, default="../Training/", help='Path to save the training and validation results')
     Parser.add_argument('--CheckpointPath', type=str, default=None, help='Checkpoint for inference/resuming training')
     Parser.add_argument('--CheckpointEpoch', type=int, default=0, help='Checkpoint epoch to resume training from')
+    Parser.add_argument('--ExperimentName', type=str, help='Name for the training experiment')
 
     Args = Parser.parse_args()
     ModelType = Args.ModelType
     DatasetPath = Args.DatasetPath # Path to the dataset folder containing 'Train' and 'Val' folders
     Epochs = Args.Epochs # Total number of epochs to run the training for
     BatchSize = Args.BatchSize # Batchsize to be used in training
+    LearningRate = Args.LearningRate # Learning rate of the optimizer
     TrainingDir = Args.TrainingDir # Path to the directory where training data has to be stored
     CheckpointPath = Args.CheckpointPath # Folder name of trianing instance in the TrainingDir 
     CheckpointEpoch = Args.CheckpointEpoch # Number of epochs for which training has been completed (will resume from CheckpointEpoch + 1)
+    ExperimentName = Args.ExperimentName
 
     if(ModelType == "Supervised"):
-        train_supervised = TrainSupervised(DatasetPath, Epochs, BatchSize, TrainingDir, CheckpointPath, CheckpointEpoch)
+        train_supervised = TrainSupervised(DatasetPath, Epochs, BatchSize, LearningRate, TrainingDir, CheckpointPath, CheckpointEpoch, ExperimentName)
         train_supervised.train()
 
 if __name__ == '__main__':