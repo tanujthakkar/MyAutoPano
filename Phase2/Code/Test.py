8e79e607d67c385614ae2c810b2f0f9a9f42bad8
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-02-21 12:33:45 -0500

    Added testing code

--- Phase2/Code/Test.py
+++ Phase2/Code/Test.py
@@ -1,182 +1,163 @@
-#!/usr/bin/env python
+#!/usr/env/bin python3
 
 """
-CMSC733 Spring 2019: Classical and Deep Learning Approaches for
-Geometric Computer Vision
-Homework 0: Alohomora: Phase 2 Starter Code
+CMSC733 Spring 2021: Classical and Deep Learning Approaches for Geometric Computer Vision
+Project 1: MyAutoPano: Phase 2
 
 
 Author(s):
-Nitin J. Sanket (nitinsan@terpmail.umd.edu)
-PhD Candidate in Computer Science,
+Tanuj Thakkar (tanuj@umd.edu)
+M. Engg Robotics
 University of Maryland, College Park
 """
 
-
-# Dependencies:
-# opencv, do (pip install opencv-python)
-# skimage, do (apt install python-skimage)
+# Importing modules
+import time
+import os
+from datetime import datetime
+import numpy as np
+from tqdm import tqdm
+import sys
+import argparse
+import cv2
+from PIL import Image
+import pandas as pd
+import traceback
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
 
 import tensorflow as tf
-import cv2
-import os
-import sys
-import glob
-import Misc.ImageUtils as iu
-import random
-from skimage import data, exposure, img_as_float
-import matplotlib.pyplot as plt
-from Network.Network import HomographyModel
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+from tensorflow.keras.optimizers import Adam
+from tensorflow.train import Checkpoint, CheckpointManager
+
+from Network.HomographyNet import HomographyNet
+from Helper import *
 from Misc.MiscUtils import *
-import numpy as np
-import time
-import argparse
-import shutil
-from StringIO import StringIO
-import string
-import math as m
-from tqdm import tqdm
-from Misc.TFSpatialTransformer import *
+from GenerateData import *
+
+# from tensorflow.python.ops.numpy_ops import np_config
+# np_config.enable_numpy_behavior()
 
 
-# Don't generate pyc codes
-sys.dont_write_bytecode = True
+class Test():
 
-def SetupAll(BasePath):
-    """
-    Inputs: 
-    BasePath - Path to images
-    Outputs:
-    ImageSize - Size of the Image
-    DataPath - Paths of all images where testing will be run on
-    """   
-    # Image Input Shape
-    ImageSize = [32, 32, 3]
-    DataPath = []
-    NumImages = len(glob.glob(BasePath+'*.jpg'))
-    SkipFactor = 1
-    for count in range(1,NumImages+1,SkipFactor):
-        DataPath.append(BasePath + str(count) + '.jpg')
+    def __init__(self, Model, DatasetPath, BatchSize, CheckpointPath):
+        self.Model = Model
+        self.DatasetPath = DatasetPath
+        self.BatchSize = BatchSize
+        self.CheckpointPath = CheckpointPath
 
-    return ImageSize, DataPath
-    
-def ReadImages(ImageSize, DataPath):
-    """
-    Inputs: 
-    ImageSize - Size of the Image
-    DataPath - Paths of all images where testing will be run on
-    Outputs:
-    I1Combined - I1 image after any standardization and/or cropping/resizing to ImageSize
-    I1 - Original I1 image for visualization purposes only
-    """
-    
-    ImageName = DataPath
-    
-    I1 = cv2.imread(ImageName)
-    
-    if(I1 is None):
-        # OpenCV returns empty list if image is not read! 
-        print('ERROR: Image I1 cannot be read')
-        sys.exit()
-        
-    ##########################################################################
-    # Add any standardization or cropping/resizing if used in Training here!
-    ##########################################################################
+        self.TestingData = None
 
-    I1S = iu.StandardizeInputs(np.float32(I1))
+    def evaluate(self):
 
-    I1Combined = np.expand_dims(I1S, axis=0)
+        running_loss = 0.0
+        inference_time = 0.0
 
-    return I1Combined, I1
-                
+        for batch in tqdm(range(int(len(self.PatchA_testing_data)/self.BatchSize))):
+            X = np.empty([0, 128, 128, 2]) # Input to the network
+            y = np.empty([0, 8]) # Ground Truth
 
-def TestOperation(ImgPH, ImageSize, ModelPath, DataPath, LabelsPathPred):
-    """
-    Inputs: 
-    ImgPH is the Input Image placeholder
-    ImageSize is the size of the image
-    ModelPath - Path to load trained model from
-    DataPath - Paths of all images where testing will be run on
-    LabelsPathPred - Path to save predictions
-    Outputs:
-    Predictions written to ./TxtFiles/PredOut.txt
-    """
-    Length = ImageSize[0]
-    # Predict output with forward pass, MiniBatchSize for Test is 1
-    _, prSoftMaxS = CIFAR10Model(ImgPH, ImageSize, 1)
+            for index in range(self.BatchSize):
+                X = np.insert(X, index, np.dstack((preprocess_image(load_image(self.PatchA_testing_data[(batch*self.BatchSize)+index])), preprocess_image(load_image(self.PatchB_testing_data[(batch*self.BatchSize)+index])))), axis=0)
+                y = np.insert(y, index, preprocess_H4_data(self.H4_testing_data[(batch*self.BatchSize)+index]), axis=0)
 
-    # Setup Saver
-    Saver = tf.train.Saver()
+            tick = time.time()
 
-    
-    with tf.Session() as sess:
-        Saver.restore(sess, ModelPath)
-        print('Number of parameters in this model are %d ' % np.sum([np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()]))
-        
-        OutSaveT = open(LabelsPathPred, 'w')
-
-        for count in tqdm(range(np.size(DataPath))):            
-            DataPathNow = DataPath[count]
-            Img, ImgOrg = ReadImages(ImageSize, DataPathNow)
-            FeedDict = {ImgPH: Img}
-            PredT = np.argmax(sess.run(prSoftMaxS, FeedDict))
-
-            OutSaveT.write(str(PredT)+'\n')
+            y_ = self.Model(X, training=False) # Prediction of the network
+            toc = time.time()
+            inference_time += (toc - tick)
             
-        OutSaveT.close()
+            loss = L2_loss(y, y_) # L2 loss between prediction (y_) and ground truth (y)
 
+            running_loss += loss
 
-def ReadLabels(LabelsPathTest, LabelsPathPred):
-    if(not (os.path.isfile(LabelsPathTest))):
-        print('ERROR: Test Labels do not exist in '+LabelsPathTest)
-        sys.exit()
-    else:
-        LabelTest = open(LabelsPathTest, 'r')
-        LabelTest = LabelTest.read()
-        LabelTest = map(float, LabelTest.split())
+            result = generateResult(self.TestingData[batch], deprocess_H4_data(y_[0].numpy()), self.H_AB_testing_data[batch], [240, 320], [128,128], 32, True)
+        # cv2.imwrite(os.path.join(self.ResultsPath, 'Val', '%d.png'%self.Epoch), result)
+        running_loss = running_loss/(len(self.PatchA_testing_data)/self.BatchSize) # Mean loss of the epoch
+        print("Testing Loss: %f"%(running_loss))
+        inference_time = inference_time/(len(self.PatchA_testing_data)/self.BatchSize)
+        print("Average Inference Time: %f"%(inference_time))
 
-    if(not (os.path.isfile(LabelsPathPred))):
-        print('ERROR: Pred Labels do not exist in '+LabelsPathPred)
-        sys.exit()
-    else:
-        LabelPred = open(LabelsPathPred, 'r')
-        LabelPred = LabelPred.read()
-        LabelPred = map(float, LabelPred.split())
+        return running_loss, inference_time
+
+    def test(self):
+
+        print("GPU %d"%tf.test.is_gpu_available()) # Checking GPU availability
+
+        self.Model.summary() # Summary of the model
+
+        if(self.CheckpointPath):
+            self.TrainingDir = os.path.join('../Training', self.CheckpointPath) # Initializing the training directory for current training instance
+            self.CheckpointPath = os.path.join(self.TrainingDir, 'Checkpoints') # Initializing checkpoint directory of current training instance
+            self.ResultsPath = os.path.join(self.TrainingDir, 'Results') # Initializing results directory of current training instance
+            print("Training Data Path: %s"%self.TrainingDir)
+            print("Checkpoints Path: %s"%self.CheckpointPath)
+            print("Results Path: %s"%self.ResultsPath)
+        else:
+            print("CHECKPOINT DIRECTORY NOT FOUND!")
+
+        learning_rate = 1E-4 # Learning rate of 0.005 from the paper
+        self.Optimizer = Adam(learning_rate=learning_rate)
+
+        # Creating a Checkpoint Manager
+        ckpt = Checkpoint(step=tf.Variable(0), model=self.Model, optimizer=self.Optimizer)
+        ckpt_manager = CheckpointManager(ckpt, self.CheckpointPath, max_to_keep=None)
+
+        # Loading Testing Data
+        self.TestingData = readImageSet('../Data/Test/')
+        self.PatchA_testing_data = readImageSet(os.path.join(self.DatasetPath, 'Test', 'PatchA')) # Gettings relative paths to PatchA testing images
+        self.PatchB_testing_data = readImageSet(os.path.join(self.DatasetPath, 'Test', 'PatchB')) # Gettings relative paths to PatchB testing images
+        with open (os.path.join(self.DatasetPath, 'Test', 'H4.npy'), 'rb') as f:
+            self.H4_testing_data = np.load(f)
+        with open (os.path.join(self.DatasetPath, 'Test', 'H_AB.npy'), 'rb') as f:
+            self.H_AB_testing_data = np.load(f)
         
-    return LabelTest, LabelPred
+        ckpt.restore(ckpt_manager.latest_checkpoint) # Restoring checkpoint if available
+        if(ckpt_manager.latest_checkpoint):
+            self.Model = ckpt.model # Restoring model state
+            print("Restored from {}".format(ckpt_manager.latest_checkpoint))
+            self.Optimizer.lr.assign(ckpt.optimizer.lr) # Restoring learning rate of the optimizer
+            print("Restored Learnign Rate: {}".format(self.Optimizer.lr))
+        else:
+            print("Initializing from scratch.")
 
-        
+        start = time.time() # Training instance start time
+
+        try:
+            testing_epoch_loss, testing_inference_time = self.evaluate() # Test the model
+
+            end = time.time()
+
+            print("Took %.03f minutes to test"%((end-start)/60))
+        except KeyboardInterrupt:
+            print(traceback.format_exc())
+
+            end = time.time()
+
+            print("Took %.03f minutes to test"%((end-start)/60))
+
+
 def main():
-    """
-    Inputs: 
-    None
-    Outputs:
-    Prints out the confusion matrix with accuracy
-    """
-
-    # Parse Command Line arguments
     Parser = argparse.ArgumentParser()
-    Parser.add_argument('--ModelPath', dest='ModelPath', default='/home/chahatdeep/Downloads/Checkpoints/144model.ckpt', help='Path to load latest model from, Default:ModelPath')
-    Parser.add_argument('--BasePath', dest='BasePath', default='/home/chahatdeep/Downloads/aa/CMSC733HW0/CIFAR10/Test/', help='Path to load images from, Default:BasePath')
-    Parser.add_argument('--LabelsPath', dest='LabelsPath', default='./TxtFiles/LabelsTest.txt', help='Path of labels file, Default:./TxtFiles/LabelsTest.txt')
+    Parser.add_argument('--ModelType', type=str, default="Supervised", help='Select between Supervised and Unsupervised model for testing (Supervised, Unsupervised)')
+    Parser.add_argument('--DatasetPath', type=str, default="../Data/Patches_50000/", help='Path to the testing dataset')
+    Parser.add_argument('--BatchSize', type=int, default=1, help='batch_size')
+    Parser.add_argument('--CheckpointPath', type=str, default=None, help='Checkpoint for testing')
+
     Args = Parser.parse_args()
-    ModelPath = Args.ModelPath
-    BasePath = Args.BasePath
-    LabelsPath = Args.LabelsPath
+    ModelType = Args.ModelType # Model to test
+    DatasetPath = Args.DatasetPath # Path to the dataset folder containing 'Train' and 'Val' folders
+    BatchSize = Args.BatchSize # Batchsize to be used in training
+    CheckpointPath = Args.CheckpointPath # Folder name of trianing instance in the TrainingDir 
 
-    # Setup all needed parameters including file reading
-    ImageSize, DataPath = SetupAll(BasePath)
+    if(ModelType == "Supervised"):
+        Model = HomographyNet() # Creating an instance of the HomographyNet model
+        test_model = Test(Model, DatasetPath, BatchSize, CheckpointPath)
+        test_model.test()
 
-    # Define PlaceHolder variables for Input and Predicted output
-    ImgPH = tf.placeholder('float', shape=(1, ImageSize[0], ImageSize[1], 3))
-    LabelsPathPred = './TxtFiles/PredOut.txt' # Path to save predicted labels
-
-    TestOperation(ImgPH, ImageSize, ModelPath, DataPath, LabelsPathPred)
-
-    # Plot Confusion Matrix
-    LabelsTrue, LabelsPred = ReadLabels(LabelsPath, LabelsPathPred)
-    ConfusionMatrix(LabelsTrue, LabelsPred)
-     
 if __name__ == '__main__':
     main()
- 
